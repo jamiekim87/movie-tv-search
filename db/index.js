1960e066e9af179378acc5af66ad76d284dd87ab
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-27 12:42:27 -0700

    push to repo

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect('mongodb://localhost/media_db', {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/media_db', {
   useNewUrlParser: true,
   useFindAndModify: true,
   useUnifiedTopology: true
